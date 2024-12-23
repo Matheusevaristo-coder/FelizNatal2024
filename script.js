47f9388dd826ad9c8ced5aa74a9a3ac9d66e20ec
Matheusevaristo-coder <math_eva1502@hotmail.com>
2024-12-23 20:33:47 -0300

    Adicionei texto 'Clique na árvore' e música natalina

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function openCurtains() {
     document.getElementById('curtain-container').classList.add('open');
     document.getElementById('message').style.display = 'block';
     document.getElementById('background-music').play();
+    document.getElementById('click-text').style.display = 'none'; // Esconder o texto
 }
